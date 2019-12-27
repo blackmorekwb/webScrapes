0e0364e23096237d9725912a003c981eab2c7297
blackmorekwb <blackmorekwb@gmail.com>
2019-12-26 20:19:31 -0600

    mostly working: i interval skips one on column switch

---
+++ fateds_extra.py
@@ -0,0 +1,61 @@
+
+"""
+    print('in batched')
+    get_prices(0,7,0)
+    print('BATCH COMPLETE')
+    get_prices(8,15,0)
+    print('BATCH COMPLETE')
+    get_prices(16,23,0)
+    print('BATCH COMPLETE')
+    get_prices(24,31,0)
+    print('BATCH COMPLETE')
+    get_prices(32,39,0)
+    print('BATCH COMPLETE')
+    get_prices(40,47,0)
+    print('BATCH COMPLETE')
+    get_prices(48,55,0)
+    print('BATCH COMPLETE')
+
+
+    print('in batched')
+    get_prices(0,7,1)
+    print('BATCH COMPLETE')
+    get_prices(8,15,1)
+    print('BATCH COMPLETE')
+    get_prices(16,23,1)
+    print('BATCH COMPLETE')
+    get_prices(24,31,1)
+    print('BATCH COMPLETE')
+    get_prices(32,39,1)
+    print('BATCH COMPLETE')
+    get_prices(40,47,1)
+    print('BATCH COMPLETE')
+    get_prices(48,55,1)
+    print('BATCH COMPLETE')
+
+    print('in batched')
+    get_prices(0,7,2)
+    print('BATCH COMPLETE')
+    get_prices(8,15,2)
+    print('BATCH COMPLETE')
+    get_prices(16,23,2)
+    print('BATCH COMPLETE')
+    get_prices(24,31,2)
+    print('BATCH COMPLETE')
+    get_prices(32,39,2)
+    print('BATCH COMPLETE')
+    get_prices(40,47,2)
+    print('BATCH COMPLETE')
+    get_prices(48,55,2)
+    print('BATCH COMPLETE')
+"""
+
+(Pdb) len(fated_uniques[2])
+15
+(Pdb) len(fated_uniques[1])
+15
+(Pdb) len(fated_uniques[0])
+17
+(Pdb)
+
+3 columns of fated list is off, will thorw off the otehr price columns
